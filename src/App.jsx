4080f0a77984a79f2e7a5db327ac1eabc9ce4658
Aradhana Kumari <aradhanakumari2602@gmail.com>
2024-09-09 14:58:53 +0530

    added navbar member

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Navbar from "./navbar/Navbar"; // Adjust path as per your directory structure
 
 function App() {
- 
   return (
-    <>
-      
-        
-    </>
-  )
+    <div className="App">
+      <Navbar /> {/* Ensure the component name matches the import */}
+      {/* Other components or content can go here */}
+    </div>
+  );
 }
 
-export default App
+export default App;
