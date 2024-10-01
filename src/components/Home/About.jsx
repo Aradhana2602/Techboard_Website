cc2dddcf3fee0408cae8acb82d420f9a3886aaa4
Aradhana Kumari <aradhanakumari2602@gmail.com>
2024-10-01 12:49:28 +0530

    new commit

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -22,7 +22,7 @@ function About() {
         }}
       ></div>
       <div className="content">
-        <h1>About Us</h1>
+        <h1>About </h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam alias eum assumenda iure nulla impedit laboriosam itaque, ea illum perspiciatis repellendus obcaecati doloremque enim perferendis soluta, veniam eius modi, quia consequuntur commodi velit minus molestias nisi nam. Tenetur quod aspernatur sed consectetur, ipsam nemo, numquam modi commodi fugiat omnis eius suscipit porro veritatis assumenda minus magnam atque ducimus asperiores quasi dicta explicabo? Natus veniam eligendi aliquam atque nulla nam aliquid rerum corporis rem tenetur, dolor incidunt nobis sunt quam veritatis! Ex blanditiis dolores ad assumenda. Nulla est repudiandae enim accusamus quae impedit, et consequatur, delectus laudantium dolorum, quaerat aspernatur dolorem?</p>
       </div>
     </div>
